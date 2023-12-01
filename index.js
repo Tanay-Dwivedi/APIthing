8973e1f2ed936de316e27f5158f5538cfe30b661
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-12-02 02:35:36 +0530

    API done

--- index.js
+++ index.js
@@ -17,17 +17,58 @@ app.use((req, res, next) => {
   next();
 });
 
+// In-memory cache
+const cache = {};
+
 // Helper function to find chapter by chID
 function findChapterByChID(chID) {
+  if (cache[chID]) {
+    // Return cached data if available
+    return cache[chID];
+  }
+
   for (const chapter in users) {
     if (users[chapter].chID == chID) {
-      return users[chapter];
+      // Cache the fetched data
+      cache[chID] = users[chapter];
+      return cache[chID];
+    }
+  }
+
+  return null;
+}
+
+// Helper function to find user by ID across all chapters
+function findUserByID(userID) {
+  for (const chapter in users) {
+    const userData = users[chapter].users.find((user) => user.id === userID);
+    if (userData) {
+      return userData;
     }
   }
   return null;
 }
 
 // Routes
+
+app.get("/api/users", (req, res) => {
+  // Check if 'id' query parameter is provided
+  const id = req.query.id;
+
+  if (id) {
+    // Search for the user with the specified ID across all chapters
+    const userData = findUserByID(Number(id));
+
+    if (userData) {
+      return res.json(userData);
+    } else {
+      return res.json({ error: "User not found" });
+    }
+  } else {
+    // Return all users if 'id' query parameter is not provided
+    return res.json(users);
+  }
+});
 
 app.get("/api/users/:chID", (req, res) => {
   const chapterId = req.params.chID;
