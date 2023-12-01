72e95f10234128a5f7d2316ff0c45a919d562de9
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-12-01 19:55:20 +0530

    added the chapters ID

--- index.js
+++ index.js
@@ -4,37 +4,55 @@ const users = require("./userData.json");
 const app = express();
 const PORT = 8000;
 
+// Middleware to parse JSON in requests
+app.use(express.json());
+
+// CORS handling - Adjust origin as needed
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Routes
 
-app.get("/users", (req, res) => {
-  const html = `
-    <ul>
-    ${Object.values(users)
-      .flat()
-      .map((user) => `<li>${user.author_name}</li>`)
-      .join("")}
-    </ul>
-    `;
-  res.send(html);
-});
-
 app.get("/api/users", (req, res) => {
-  const allUsers = Object.values(users).flat();
-  return res.json(allUsers);
+  return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    let foundChapter = null;
+app.get("/api/users/:chapter", (req, res) => {
+  const chapterId = req.params.chapter;
+  const chapterData = users[chapterId];
 
-    Object.keys(users).forEach(chapterKey => {
-        const chapter = users[chapterKey];
-        const user = chapter.find(user => user.id === id);
-        if (user) foundChapter = chapter;
-    });
-
-    return res.json(foundChapter || { error: "Chapter not found" });
+  if (chapterData) {
+    return res.json({ [chapterId]: chapterData });
+  } else {
+    return res.json({ error: "Chapter not found" });
+  }
 });
 
+app.get("/api/users/:chapter/:id", (req, res) => {
+  const chapterId = req.params.chapter;
+  const userId = Number(req.params.id);
 
-app.listen(PORT, () => console.log("Server running at: ${PORT}"));
+  const chapterData = users[chapterId];
+
+  if (chapterData) {
+    const userData = chapterData.find((user) => user.id === userId);
+    if (userData) {
+      return res.json({ [chapterId]: [userData] });
+    } else {
+      return res.json({ error: "User not found in the specified chapter" });
+    }
+  } else {
+    return res.json({ error: "Chapter not found" });
+  }
+});
+
+// Start the server
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
